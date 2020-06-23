7b8121e9a7960d949aa0849fdfe1ea1b60b76329
xavier <xavierhoskins@outlook.com>
2020-06-23 18:13:03 -0500

    finished basic layout of the page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Welcome to Xavier's Game of Life</header>
+      <div className="app-container">
+        <div className="canvasSection">
+          <h1>Generation :</h1>
+
+          <div className="gameSection">
+            <canvas id="game" className="canvas"></canvas>
+            <div className="buttonSide">
+              <button>Preset 1</button>
+              <button>Preset 2</button>
+              <button>Preset 3</button>
+              <button>Preset 4</button>
+            </div>
+          </div>
+
+          <div className="buttonBottom">
+            <button>Play</button>
+            <button>Pause</button>
+            <button>Stop</button>
+          </div>
+        </div>
+
+        <div className="rulesSection">
+          <h1>Rules :</h1>
+        </div>
+      </div>
     </div>
   );
 }
